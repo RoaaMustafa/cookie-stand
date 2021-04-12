42648c8458b3138a68d5c013f76ac401225cb900
RoaaMustafa <labushanab14@gmail.com>
2021-04-12 03:49:02 +0300

    add object5

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 'use strict';
+function getRandome(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 //  ---------Seattle---------
 const seattle ={
-  locationName:'Seatle',
+  locationName:'Seattle',
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
@@ -9,7 +13,7 @@ const seattle ={
   cookiesPerHr:[],
   totalCookiesPerHr:0,
   cookiesEachHour: function () {
-    for (let n = 0; n < 14; n++) {
+    for (let n = 0; n < this.workHrs.length; n++) {
       this.cookiesPerHr.push(Math.ceil(getRandome(this.minCust,this.maxCust) * this.avgCookies));
       this.totalCookiesPerHr +=this.cookiesPerHr[n];
     }
@@ -21,7 +25,7 @@ const seattle ={
     p.textContent = this.locationName;
     let unorderedList = document.createElement('ul');
     container.appendChild(unorderedList);
-    for (let j = 0; j < 14; j++) {
+    for (let j = 0; j < this.workHrs.length; j++) {
       let li = document.createElement('li');
       unorderedList.appendChild(li);
       li.textContent = `${this.workHrs[j]}: ${this.cookiesPerHr[j]} Cookies`;
@@ -36,12 +40,10 @@ const seattle ={
 seattle.cookiesEachHour();
 seattle.createList();
 
-function getRandome(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-console.log(seattle.cookiesEachHour());
-console.log(seattle.cookiesPerHr);
-console.log(seattle.totalCookiesPerHr);
+
+// console.log(seattle.cookiesEachHour());
+// console.log(seattle.cookiesPerHr);
+// console.log(seattle.totalCookiesPerHr);
 
 //  ---------Tokyo---------
 const tokyo ={
@@ -53,7 +55,7 @@ const tokyo ={
   cookiesPerHr:[],
   totalCookiesPerHr:0,
   cookiesEachHour: function () {
-    for (let n = 0; n < 14; n++) {
+    for (let n = 0; n < this.workHrs.length; n++) {
       this.cookiesPerHr.push(Math.ceil(getRandome(this.minCust,this.maxCust) * this.avgCookies));
       this.totalCookiesPerHr +=this.cookiesPerHr[n];
     }
@@ -65,7 +67,7 @@ const tokyo ={
     p.textContent = this.locationName;
     let unorderedList = document.createElement('ul');
     container.appendChild(unorderedList);
-    for (let j = 0; j < 14; j++) {
+    for (let j = 0; j < this.workHrs.length; j++) {
       let li = document.createElement('li');
       unorderedList.appendChild(li);
       li.textContent = `${this.workHrs[j]}: ${this.cookiesPerHr[j]} Cookies`;
@@ -80,9 +82,9 @@ const tokyo ={
 tokyo.cookiesEachHour();
 tokyo.createList();
 
-console.log(tokyo.cookiesEachHour());
-console.log(tokyo.cookiesPerHr);
-console.log(tokyo.totalCookiesPerHr);
+// console.log(tokyo.cookiesEachHour());
+// console.log(tokyo.cookiesPerHr);
+// console.log(tokyo.totalCookiesPerHr);
 
 
 //  ---------Dubai---------
@@ -95,7 +97,7 @@ const Dubai ={
   cookiesPerHr:[],
   totalCookiesPerHr:0,
   cookiesEachHour: function () {
-    for (let n = 0; n < 14; n++) {
+    for (let n = 0; n < this.workHrs.length; n++) {
       this.cookiesPerHr.push(Math.ceil(getRandome(this.minCust,this.maxCust) * this.avgCookies));
       this.totalCookiesPerHr +=this.cookiesPerHr[n];
     }
@@ -107,7 +109,7 @@ const Dubai ={
     p.textContent = this.locationName;
     let unorderedList = document.createElement('ul');
     container.appendChild(unorderedList);
-    for (let j = 0; j < 14; j++) {
+    for (let j = 0; j < this.workHrs.length; j++) {
       let li = document.createElement('li');
       unorderedList.appendChild(li);
       li.textContent = `${this.workHrs[j]}: ${this.cookiesPerHr[j]} Cookies`;
@@ -121,9 +123,9 @@ const Dubai ={
 };
 Dubai.cookiesEachHour();
 Dubai.createList();
-console.log(Dubai.cookiesEachHour());
-console.log(Dubai.cookiesPerHr);
-console.log(Dubai.totalCookiesPerHr);
+// console.log(Dubai.cookiesEachHour());
+// console.log(Dubai.cookiesPerHr);
+// console.log(Dubai.totalCookiesPerHr);
 
 
 
@@ -137,9 +139,12 @@ const Paris ={
   cookiesPerHr:[],
   totalCookiesPerHr:0,
   cookiesEachHour: function () {
-    for (let n = 0; n < 14; n++) {
+    for (let n = 0; n < this.workHrs.length; n++) {
       this.cookiesPerHr.push(Math.ceil(getRandome(this.minCust,this.maxCust) * this.avgCookies));
       this.totalCookiesPerHr +=this.cookiesPerHr[n];
+
+
+
     }
   },
   createList: function () {
@@ -149,7 +154,7 @@ const Paris ={
     p.textContent = this.locationName;
     let unorderedList = document.createElement('ul');
     container.appendChild(unorderedList);
-    for (let j = 0; j < 14; j++) {
+    for (let j = 0; j < this.workHrs.length; j++) {
       let li = document.createElement('li');
       unorderedList.appendChild(li);
       li.textContent = `${this.workHrs[j]}: ${this.cookiesPerHr[j]} Cookies`;
@@ -163,10 +168,48 @@ const Paris ={
 };
 Paris.cookiesEachHour();
 Paris.createList();
-console.log(Paris.cookiesEachHour());
-console.log(Paris.cookiesPerHr);
-console.log(Paris.totalCookiesPerHr);
+// console.log(Paris.cookiesEachHour());
+// console.log(Paris.cookiesPerHr);
+// console.log(Paris.totalCookiesPerHr);
 
 
-
+//  ---------Lima---------
+const lima ={
+  locationName:'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  workHrs: ['6am','7am','8am','9am','10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm','7pm'],
+  cookiesPerHr:[],
+  totalCookiesPerHr:0,
+  cookiesEachHour: function () {
+    for (let n = 0; n < this.workHrs.length; n++) {
+      this.cookiesPerHr.push(Math.ceil(getRandome(this.minCust,this.maxCust) * this.avgCookies));
+      this.totalCookiesPerHr +=this.cookiesPerHr[n];
+    }
+  },
+  createList: function () {
+    let container =document.getElementById('cookiesSales');
+    let p = document.createElement('p');
+    container.appendChild(p);
+    p.textContent = this.locationName;
+    let unorderedList = document.createElement('ul');
+    container.appendChild(unorderedList);
+    for (let j = 0; j < this.workHrs.length; j++) {
+      let li = document.createElement('li');
+      unorderedList.appendChild(li);
+      li.textContent = `${this.workHrs[j]}: ${this.cookiesPerHr[j]} Cookies`;
+    }
+    let li1 = document.createElement('li');
+    unorderedList.appendChild(li1);
+    li1.textContent= `Total: ${this.totalCookiesPerHr} Cookies`;
+    let breakline = document.createElement('hr');
+    container.appendChild(breakline);
+  }
+};
+lima.cookiesEachHour();
+lima.createList();
+// console.log(lima.cookiesEachHour());
+// console.log(lima.cookiesPerHr);
+// console.log(lima.totalCookiesPerHr);
 
