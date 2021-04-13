25c8b4a0cbc09037e89ebc9b4ca6f4d42f7c6a96
RoaaMustafa <labushanab14@gmail.com>
2021-04-13 10:42:29 +0300

    make updates

--- js/app.js
+++ js/app.js
@@ -3,8 +3,7 @@
 
 
 // // constructing a function
-// let arrayOfLocattions = [];
-// let workHrs= ['6am','7am','8am','9am','10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm','7pm'],
+
 
 let workHrs = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 //helper function
@@ -96,6 +95,7 @@ function renderFooter() {
   tdEl.textContent = 'Total';
   let hourTotal;
   let totalForAllLocations = 0;
+  //to calc total in 
   for (let h = 0; h < workHrs.length; h++) {
     hourTotal = 0;
     for (let p = 0; p < locationsArray.length; p++) {
